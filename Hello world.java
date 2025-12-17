349adf4ffb0a06bd36252e780c4374911c0bf23f
eberegovaya <beregovajaelena123@gmail.com>
2025-12-17 23:47:36 +0300

    message2

--- Hello world.java
+++ Hello world.java
@@ -1,11 +1,2 @@
-public
-class Main {
+System.out.println("Hello world!") ;
 
-    public static void main(String[] args) {
-
-        System.out.println("Hello world!")
-        System.out.println("Hello world!") ;
-
-    }
-
-}
