9f7db5a952f24f367fb6ba96d5dd682e36da551c
eberegovaya <beregovayaelena123@gmail.com>
2025-12-21 17:23:11 +0300

    come back 2 message

--- Hello world.java
+++ Hello world.java
@@ -1,2 +1,7 @@
-System.out.println("Hello world!") ;
+public class Main {
+    public static void main (String[] args) {
+        System.out.println(Hello world!);
+        System.out.println(Hello world!);
 
+    }
+}
